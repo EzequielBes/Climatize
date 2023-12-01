3095b3dfd99a4fb0f86d05f811cf87b827e819a2
EzequielBes <ezequieltdbeserra@gmail.com>
2023-12-01 15:10:26 -0300

    feat: add api and more

--- src/components/Clima.tsx
+++ src/components/Clima.tsx
@@ -1,14 +1,38 @@
-import { Box, Flex, Image, Input, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  Image,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { getCity } from "@/pages/api/hello";
 
 export function Clima() {
-  const [city, setCity] = useState("Sp");
+  const [racity, setCity] = useState();
+  const [cidade, setcidade] = useState<string>();
+  const [garantia, setgarantia] = useState();
+
+  function fazerCidade(event: any) {
+    setcidade(event.target.value);
+  }
+
+  async function handleCity() {
+    setgarantia((await getCity(cidade)).data);
+    console.log(garantia);
+  }
+
+  useEffect(() => {
+    handleCity;
+  }, [fazerCidade]);
 
   return (
     <Box>
       <Flex>
         <Box>
-          <Text fontFamily={"poppins"}>Clima: {city}</Text>
+          <Text fontFamily={"poppins"}>Clima: {}</Text>
           <Box w="50vw" h="40vh" bg="teal.200" borderRadius={20} boxShadow="lg">
             <Flex
               flexDir={"column"}
@@ -18,10 +42,18 @@ export function Clima() {
             >
               <img src="" />
               imagem aqui
-              <Text>Temperatura aqui</Text>
+              <Text>{racity}</Text>
             </Flex>
             <Flex w="30%" margin={"0 auto"} h="0%" align={"center"}>
-              <Input type="text" name="cidade" bg={"white"} />
+              <FormControl>
+                <Input
+                  type="text"
+                  name="cidade"
+                  bg={"white"}
+                  onChange={fazerCidade}
+                />
+                <Button onClick={handleCity}>enviar </Button>
+              </FormControl>
             </Flex>
           </Box>
         </Box>
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,2 +1,17 @@
+import axios from "axios";
+
 const apikey = "32d3d425093a322d51caeccf86680295";
 const apiCountry = "https://countryflagsapi.com/png/br";
+
+export const dados = axios
+  .get(
+    "https://api.openweathermap.org/data/2.5/weather?q=são paulo&appid=32d3d425093a322d51caeccf86680295"
+  )
+  .then((response) => response.data);
+
+export const url = axios.create({
+  baseURL: "https://api.openweathermap.org/data/2.5",
+});
+
+export const getCity = (name: string) =>
+  url.get("/weather?q=" + name + "&appid=32d3d425093a322d51caeccf86680295");
