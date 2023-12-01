7c815de79ad7fd6cf1b1ac1a152cee0eb98c4db6
EzequielBes <ezequieltdbeserra@gmail.com>
2023-12-01 13:34:28 -0300

    feat: parte da estilização feita

--- src/components/Clima.tsx
+++ src/components/Clima.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 export function Clima() {
@@ -8,7 +8,22 @@ export function Clima() {
     <Box>
       <Flex>
         <Box>
-          <Text>Clima: {city}</Text>
+          <Text fontFamily={"poppins"}>Clima: {city}</Text>
+          <Box w="50vw" h="40vh" bg="teal.200" borderRadius={20} boxShadow="lg">
+            <Flex
+              flexDir={"column"}
+              h="80%"
+              justify={"center"}
+              align={"center"}
+            >
+              <img src="" />
+              imagem aqui
+              <Text>Temperatura aqui</Text>
+            </Flex>
+            <Flex w="30%" margin={"0 auto"} h="0%" align={"center"}>
+              <Input type="text" name="cidade" bg={"white"} />
+            </Flex>
+          </Box>
         </Box>
       </Flex>
     </Box>
