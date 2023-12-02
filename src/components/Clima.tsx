a088a136afbd516ec41bd4f42847f3e0c364a356
EzequielBes <ezequieltdbeserra@gmail.com>
2023-12-02 12:37:32 -0300

    feat: Part of the interface finished

--- src/components/Clima.tsx
+++ src/components/Clima.tsx
@@ -4,35 +4,48 @@ import {
   Flex,
   FormControl,
   Image,
+  Img,
   Input,
   Text,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { getCity } from "@/pages/api/hello";
+import { setConfig } from "next/config";
 
 export function Clima() {
   const [racity, setCity] = useState();
-  const [cidade, setcidade] = useState<string>();
+  const [cidade, setcidade] = useState("");
   const [garantia, setgarantia] = useState();
+
+  const [cityName, setCityName] = useState("...");
+  const [cityTemp, setTempCity] = useState();
+  const [cityDesc, setCityDesc] = useState();
+  const [iconPadrao, setIconPadrao] = useState();
+
+  const [sendConfi, setSendConf] = useState(false);
 
   function fazerCidade(event: any) {
     setcidade(event.target.value);
   }
 
   async function handleCity() {
-    setgarantia((await getCity(cidade)).data);
-    console.log(garantia);
+    if (await getCity(cidade)) {
+      setConfig(true);
+      setgarantia((await getCity(cidade)).data);
+      if (garantia !== undefined) {
+        setCityName(garantia.name);
+        setTempCity(garantia.main.temp);
+        setCityDesc(garantia.weather[0].description);
+        setIconPadrao(garantia.weather[0].icon);
+      }
+    }
   }
-
-  useEffect(() => {
-    handleCity;
-  }, [fazerCidade]);
 
   return (
     <Box>
       <Flex>
         <Box>
-          <Text fontFamily={"poppins"}>Clima: {}</Text>
+          <Text fontFamily={"poppins"}>Clima: {cityName} </Text>
           <Box w="50vw" h="40vh" bg="teal.200" borderRadius={20} boxShadow="lg">
             <Flex
               flexDir={"column"}
@@ -40,12 +53,17 @@ export function Clima() {
               justify={"center"}
               align={"center"}
             >
-              <img src="" />
-              imagem aqui
-              <Text>{racity}</Text>
+              <Text fontFamily={"poppins"} fontSize={30}>
+                {Math.round(cityTemp / 10)} graus
+              </Text>
+              <Text fontSize={14} fontFamily={"poppins"}>
+                Descrição:{cityDesc}{" "}
+              </Text>
+              <img src={`http://openweathermap.org/img/wn/${iconPadrao}.png`} />
             </Flex>
+
             <Flex w="30%" margin={"0 auto"} h="0%" align={"center"}>
-              <FormControl>
+              <FormControl display={"flex"}>
                 <Input
                   type="text"
                   name="cidade"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { FaCloudMoonRain } from "react-icons/fa6";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Icon, Text } from "@chakra-ui/react";
 import { Clima } from "@/components/Clima";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,14 @@ export default function Home() {
       <Box w="80vw" margin="0 auto">
         <Flex h="15vh" w="100%" align={"center"}>
           <Text borderBottom={"1px solid black"}>Climatize</Text>
+          <Icon
+            color={"yellow"}
+            ml="10"
+            fontSize={24}
+            justifyContent={"center"}
+          >
+            <FaCloudMoonRain />
+          </Icon>
         </Flex>
         <Clima />
       </Box>
